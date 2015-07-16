4cf54ae75da5d0345573816c1fc2f9fe1a6e6408
unknown <divyank963@98GDT12.IN.nam.ad.pwcinternal.com>
2015-07-16 17:22:30 +0530

    First commit

---
+++ Week2 Project/Scripts/Complete.R
@@ -0,0 +1,17 @@
+##Divyan Kavdia's Complete
+
+##directory<-c('F:/Coursera/Week2 Project/specdata')
+##id<-1:332
+
+complete <- function(directory, id = 1:332) {
+   allFiles <- list.files(path = directory, full.names = TRUE)
+  selectedData <- data.frame()
+  completeCases <- data.frame()
+  nobs <- data.frame();
+  for (i in id) { 
+    selectedData <- (read.csv(allFiles[i],header=TRUE))
+    nobs <- sum(complete.cases(selectedData))
+    completeCases <- rbind(completeCases, data.frame(i,nobs))
+  }
+  completeCases
+}
